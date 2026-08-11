7b54078c3588b26230724a0940a0bc92a24e631e
julieannawish <julieannawish@gmail.com>
2026-08-11 14:48:23 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,9 +1,22 @@
-const CACHE='malgussi-v3-18-smart-relink-scroll';
-self.addEventListener('install',e=>{self.skipWaiting()});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
+const CACHE='malgussi-v3-21-dog-album-scroll';
+const CORE=['./','./index.html','./manifest.webmanifest','./dog_default.png','./app-icon.png','./app-icon-192.png'];
+self.addEventListener('install',e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting()));
+});
+self.addEventListener('activate',e=>{
+  e.waitUntil(caches.keys().then(keys=>Promise.all(
+    keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))
+  )).then(()=>self.clients.claim()));
+});
 self.addEventListener('fetch',e=>{
   if(e.request.method!=='GET')return;
-  e.respondWith(fetch(e.request).then(r=>{
-    const copy=r.clone(); caches.open(CACHE).then(c=>c.put(e.request,copy)); return r;
-  }).catch(()=>caches.match(e.request)));
+  e.respondWith(
+    caches.match(e.request).then(cached=>{
+      const network=fetch(e.request).then(r=>{
+        if(r.ok) caches.open(CACHE).then(c=>c.put(e.request,r.clone()));
+        return r;
+      }).catch(()=>cached);
+      return cached || network;
+    })
+  );
 });
