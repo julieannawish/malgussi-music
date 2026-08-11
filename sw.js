5e6edb2dd23d7e4a881a5fe0f8a3cfbd0181d32d
julieannawish <julieannawish@gmail.com>
2026-08-11 17:24:20 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='malgussi-v3-23-checked';
+const CACHE='malgussi-v3-24-checked';
 const CORE=['./','./index.html','./manifest.webmanifest','./dog_default.png','./app-icon.png','./app-icon-192.png'];
 
 self.addEventListener('install', event => {
