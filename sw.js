da876e87b74c92f84d5e1ff99e2b6863b7d46ef1
julieannawish <julieannawish@gmail.com>
2026-08-11 12:30:57 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='malgussi-v3-11';
+const CACHE='malgussi-v3-12';
 self.addEventListener('install',e=>{self.skipWaiting()});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
 self.addEventListener('fetch',e=>{
