e3b73eceeab982a1d78f8e09563bcbb1ecf519ff
julieannawish <julieannawish@gmail.com>
2026-08-11 11:56:17 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,9 @@
+const CACHE='malgussi-v3-11';
+self.addEventListener('install',e=>{self.skipWaiting()});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
+self.addEventListener('fetch',e=>{
+  if(e.request.method!=='GET')return;
+  e.respondWith(fetch(e.request).then(r=>{
+    const copy=r.clone(); caches.open(CACHE).then(c=>c.put(e.request,copy)); return r;
+  }).catch(()=>caches.match(e.request)));
+});
