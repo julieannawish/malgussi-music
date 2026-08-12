0c3eb1c7086ed93b474e8cf1ca1a9255820df6e5
julieannawish <julieannawish@gmail.com>
2026-08-12 09:44:55 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='malgussi-v3-44-pip-ui';
+const CACHE='malgussi-v3-45-airplay-pip';
 const CORE=['./','./index.html','./manifest.webmanifest','./dog_default.png','./app-icon.png','./app-icon-192.png'];
 
 self.addEventListener('message', event=>{
