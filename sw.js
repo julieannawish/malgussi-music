f93fa6c68c5c4eee6965c6580122fcc40f1adb13
julieannawish <julieannawish@gmail.com>
2026-08-15 14:17:42 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='malgussi-v431';
+const CACHE='malgussi-v432';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./dog_default.png','./profile_default.png','./app-icon.png','./app-icon-192.png','./app-icon-maskable.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 // V4.26: only purge OLD APP-SHELL caches (malgussi-vNNN). NEVER delete the track
